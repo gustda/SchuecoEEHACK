15e28e9eb3524e081401feba1f38eb36ba6edd85
Daniel Gustorff <d.gustorff@gmail.com>
2018-01-30 16:28:59 +0100

    Democlient und Hotelroom funktional fertig, ToDo aufräumen und vereinfachen

--- SchuecoEEHACK/HotelRoom.cs
+++ SchuecoEEHACK/HotelRoom.cs
@@ -11,14 +11,26 @@ namespace SchuecoEEHACK
     {
         public int Number { get; private set; }
         public double AmbientTemperature { get; set; }
+        public double RoomTemperature { get; set; }
+        public string PresenceState { get; set; }
         public string SunState { get; set; }
         public string AirState { get; set; }
+        public string LightState { get; set;}
+        public string WindowState { get; set; }
+        public string HeaterState { get; set; }
+        public string BlindState { get; set; }
         public HotelRoom(int number)
         {
             Number = number;
             AmbientTemperature = 15;
+            RoomTemperature = 18;
+            PresenceState = "Unused";
             SunState = "Cloudy";
             AirState = "Used Air";
+            LightState = "Off";
+            WindowState = "Closed";
+            HeaterState = "On";
+            BlindState = "Open";
         }
     }
 }
--- SchuecoEEHACK/MicrosoftWebSockets.cs
+++ SchuecoEEHACK/MicrosoftWebSockets.cs
@@ -23,9 +23,7 @@ namespace SchuecoEEHACK
         {
             // wenn eine neue WebSocket verbindung hergestellt wird, wird zuerst die Ziel IP der Steuerung und der Clientname ausgelesen
             // für eine Produktivversion sollte hierzu eine Verbindungsanfrage gesendet werden, dazu ist ein bisschen Protokoll notwendig
-            var connectionData = this.WebSocketContext.QueryString["ConnectionData"];         
-            connectionData = connectionData.Substring(connectionData.IndexOf('=') + 1);
-
+            
             // der neu angelegte Client wird in die WebSocket client Liste aufgenommen
             clients.Add(this);            
 
@@ -83,6 +81,30 @@ namespace SchuecoEEHACK
                                 SendValue("ambient_temperature");
                                 break;
 
+                            case "room_temperature":
+                                var room_temperature = Convert.ToDouble(set_value.value);
+                                foreach(var room in Rooms)
+                                {
+                                    if(room.Number==RoomNumber)
+                                    {
+                                        room.RoomTemperature = room_temperature;
+                                    }
+                                }
+                                SendValue("room_temperature");
+                                break;
+
+                            case "presence_state":
+                                var presence_state = set_value.value.ToString();
+                                foreach (var room in Rooms)
+                                {
+                                    if (room.Number == RoomNumber)
+                                    {
+                                        room.PresenceState = presence_state;
+                                    }
+                                }
+                                SendValue("presence_state");
+                                break;
+
                             case "sun_state":
                                 var sun_state = set_value.value.ToString();
                                 foreach (var room in Rooms)
@@ -104,6 +126,54 @@ namespace SchuecoEEHACK
                                     }
                                 }
                                 SendValue("air_state");
+                                break;
+
+                            case "light_state":
+                                var light_state = set_value.value.ToString();
+                                foreach (var room in Rooms)
+                                {
+                                    if (room.Number == RoomNumber)
+                                    {
+                                        room.LightState = light_state;
+                                    }
+                                }
+                                SendValue("light_state");
+                                break;
+
+                            case "window_state":
+                                var window_state = set_value.value.ToString();
+                                foreach (var room in Rooms)
+                                {
+                                    if (room.Number == RoomNumber)
+                                    {
+                                        room.WindowState = window_state;
+                                    }
+                                }
+                                SendValue("window_state");
+                                break;
+
+                            case "heater_state":
+                                var heater_state = set_value.value.ToString();
+                                foreach (var room in Rooms)
+                                {
+                                    if (room.Number == RoomNumber)
+                                    {
+                                        room.HeaterState = heater_state;
+                                    }
+                                }
+                                SendValue("heater_state");
+                                break;
+
+                            case "blind_state":
+                                var blind_state = set_value.value.ToString();
+                                foreach (var room in Rooms)
+                                {
+                                    if (room.Number == RoomNumber)
+                                    {
+                                        room.BlindState = blind_state;
+                                    }
+                                }
+                                SendValue("blind_state");
                                 break;
                         }
                         break;
@@ -167,6 +237,18 @@ namespace SchuecoEEHACK
                     SendJsonToAllClients(json);
                     break;
 
+                case "room_temperature":
+                    var roomTemp = new Json.SetValue() { type = "property_update", value_name = "room_temperature", value = GetRoom().RoomTemperature };
+                    json = JsonConvert.SerializeObject(roomTemp);
+                    SendJsonToAllClients(json);
+                    break;
+
+                case "presence_state":
+                    var presenceState = new Json.SetValue() { type = "property_update", value_name = "presence_state", value = GetRoom().PresenceState};
+                    json = JsonConvert.SerializeObject(presenceState);
+                    SendJsonToAllClients(json);
+                    break;
+
                 case "sun_state":
                     var sunState = new Json.SetValue() { type = "property_update", value_name = "sun_state", value = GetRoom().SunState };
                      json = JsonConvert.SerializeObject(sunState);
@@ -178,6 +260,31 @@ namespace SchuecoEEHACK
                     json = JsonConvert.SerializeObject(airState);
                     SendJsonToAllClients(json);
                     break;
+                    
+                case "light_state":
+                    var lightState = new Json.SetValue() { type = "property_update", value_name = "light_state", value = GetRoom().LightState };
+                    json = JsonConvert.SerializeObject(lightState);
+                    SendJsonToAllClients(json);
+                    break;
+
+                case "window_state":
+                    var windowState = new Json.SetValue() { type = "property_update", value_name = "window_state", value = GetRoom().WindowState };
+                    json = JsonConvert.SerializeObject(windowState);
+                    SendJsonToAllClients(json);
+                    break;
+
+                case "heater_state":
+                    var heaterState = new Json.SetValue() { type = "property_update", value_name = "heater_state", value = GetRoom().HeaterState };
+                    json = JsonConvert.SerializeObject(heaterState);
+                    SendJsonToAllClients(json);
+                    break;
+
+                case "blind_state":
+                    var blindState = new Json.SetValue() { type = "property_update", value_name = "blind_state", value = GetRoom().BlindState };
+                    json = JsonConvert.SerializeObject(blindState);
+                    SendJsonToAllClients(json);
+                    break;
+                  
             }
         }
 
@@ -199,10 +306,24 @@ namespace SchuecoEEHACK
             {
                 var ambientTemp = new Json.SetValue() { type= "property_update", value_name = "ambient_temperature", value = room.AmbientTemperature };
                 Send(JsonConvert.SerializeObject(ambientTemp));
+                var roomTemp = new Json.SetValue() { type = "property_update", value_name = "room_temperature", value = room.RoomTemperature };
+                Send(JsonConvert.SerializeObject(roomTemp));
+                var presenceState = new Json.SetValue() { type = "property_update", value_name = "presence_state", value = room.PresenceState };
+                Send(JsonConvert.SerializeObject(presenceState));
                 var sunState = new Json.SetValue() { type = "property_update", value_name = "sun_state", value = room.SunState };
                 Send(JsonConvert.SerializeObject(sunState));
                 var airState = new Json.SetValue() { type = "property_update", value_name = "air_state", value = room.AirState};
                 Send(JsonConvert.SerializeObject(airState));
+
+                var lightState = new Json.SetValue() { type = "property_update", value_name = "light_state", value = room.LightState };
+                Send(JsonConvert.SerializeObject(lightState));
+                var windowState = new Json.SetValue() { type = "property_update", value_name = "window_state", value = room.WindowState };
+                Send(JsonConvert.SerializeObject(windowState));
+                var heaterState = new Json.SetValue() { type = "property_update", value_name = "heater_state", value = room.HeaterState };
+                Send(JsonConvert.SerializeObject(heaterState));
+                var blindState = new Json.SetValue() { type = "property_update", value_name = "blind_state", value = room.BlindState};
+                Send(JsonConvert.SerializeObject(blindState));
+
             }
         }
     }
